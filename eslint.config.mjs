0573c7e38e5c173c249ca4605ca2e26050548fc3
Jochen Metzinger <jocmet13@gmail.com>
2025-05-23 17:58:49 +0200

    format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,12 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {files: ['**/*.{js,mjs,cjs,ts,vue}'], languageOptions: {globals: {...globals.browser, ...globals.node}}},
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    languageOptions: {
+      globals: {...globals.browser, ...globals.node},
+    },
+  },
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
